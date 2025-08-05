d55ae1556e28bbac31ae8a6ef8b0c2e5ccdcc78d
PadmaDhakappaERL <padma.dhakappa@indusschool.com>
2025-08-05 10:54:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-#MONGO_URI = "mongodb+srv://admin:admin@deveagelcluster.crhta.mongodb.net/?retryWrites=true&w=majority"
 MONGO_URI = st.secrets["MONGO_URI"]
 DB_NAME = "student_db"
 COLLECTION_NAME = "questionnaires"
+
